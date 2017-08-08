296ba9c056762dc10055b4c3d2310f78b8ebb575
saraharless <sara.e.harless@gmail.com>
2017-08-08 15:21:55 -0500

    initial

---
+++ main.js
@@ -0,0 +1,43 @@
+console.log('ghost toast');
+
+let container = document.querySelector('.container')
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+button.addEventListener('click', function() {
+  console.log(input.value);
+  goFetch('taco')
+})
+
+function goFetch(search) {
+  fetch('http://recipepuppyproxy.herokuapp.com/api/?q=' + search)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Oops! Looks like there was a problem, Status Code: ' + response.status);
+          return;
+        }
+        response.json().then(function(data){
+          console.log(data);
+          for (var i = 0; i < data.results.length; i++) {
+            let image = data.results[i].thumbnail;
+            if (image === ''){
+              image = 'http://via.placeholder.com/120x90'
+            }
+            let template = `
+            <div class="recipe">
+            <img src="${image}" alt="">
+            <h3>${data.results[i].title}</h3>
+            <a href=${data.results[i].href}>Get the recipe!</a>
+            </div>
+            `
+
+            container.innerHTML += template;
+
+          }
+
+        });
+
+        }
+
+      )
+    }
